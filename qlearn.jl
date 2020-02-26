2933dfb496ec496963eac714065748a32455903c
Bjørn Remseth <la3lma@gmail.com>
2020-02-26 01:44:29 +0100

    Most of the places may now be in place, but not in the right place .-)   Throwing an exception when running training.

--- qlearn.jl
+++ qlearn.jl
@@ -16,7 +16,7 @@ one_hot_encode_coordinate_pair(source::Coord, destination::Coord) =
                    onehot(destination, all_chessboard_locations)))
 
 @test length(one_hot_encode_coordinate_pair(Coord(1,1), Coord(2,2))) == 128
-             
+
 one_hot_encode_move(m::Move) = one_hot_encode_coordinate_pair(m.start, m.destination)
 
 one_hot_encode_piece(p::ChessPiece) = onehot(p, all_chess_pieces)
@@ -29,16 +29,16 @@ end
 @test length(one_hot_encode_board(startingBoard)) ==  (13 * 64)
 
 
-one_hot_encode_chess_state(state, move_being_queried, action_history, state_history) =
+one_hot_encode_chess_state(state, move_being_queried, action_history = nothing , state_history = nothing) =
      collect(Iterators.flatten([one_hot_encode_board(state), one_hot_encode_move(move_being_queried)]))
 
-             
-@test length(one_hot_encode_chess_state(startingBoard, Move(Coord(1,1), Coord(2,2), false, bp, bp), nothing, nothing)) == 960
+
+@test length(one_hot_encode_chess_state(startingBoard, Move(Coord(1,1), Coord(2,2), false, bp, bp))) == 960
 
 
-##
-##  Q-learning mechanics
-##
+###
+###  Q-learning mechanics
+###
 
 
 
@@ -47,22 +47,50 @@ struct Q_learning_state
     cache::Dict
 end
 
+function wipe_cache!(qs::Q_learning_state)
+    for k  in keys(qs.cache)
+        delete!(qs.cache, k)
+    end
+end
 
-# Get an action-selection function connected to a q-learning instance.
+##
+##  One-hot coding of Q-values
+##
 
-function raw_q_lookup(qs::Q_learning_state, encoded_state)
-    if haskey(qs.cache, encoded_state)
-        return qs.cache[encoded_state]
+no_of_output_nodes_to_encode_q=100
+
+function one_hot_encode_q(x)
+    idx = trunc(Int, no_of_output_nodes_to_encode_q * (x+1)/2)
+    result = zeros(no_of_output_nodes_to_encode_q)
+    result[idx] = 1
+    return result
+end
+
+function one_cold_encode_q(x)
+    idx = argmax(x)
+    return  2*(idx/no_of_output_nodes_to_encode_q) - 1
+end
+
+@test one_cold_encode_q(one_hot_encode_q(0.0)) == 0.0
+
+
+function raw_q_lookup(qs::Q_learning_state, encoded_statemove)
+    if haskey(qs.cache, encoded_statemove)
+        return qs.cache[encoded_statemove]
     else
-        result = argmax(qs.chain(encoded_state))
+        # Some scaling magic on the result to keep it between
+        # -1 and 1
+        result = 2*(argmax(qs.chain(encoded_statemove))/no_of_output_nodes_to_encode_q) - 1.0
+
+
         println("Result = ", result)
-        qs.cache[encoded_state] = result
+        qs.cache[encoded_statemove] = result
         return result
     end
 end
 
 get_q_value(qs, state, move, action_history, state_history) =
-    raw_q_lookup(qs, one_hot_encode_chess_state(state, move, action_history, state_history))
+    raw_q_lookup(qs, one_hot_encode_chess_state(state, move))
 
 
 function new_q_choice(qs::Q_learning_state)
@@ -74,17 +102,17 @@ function new_q_choice(qs::Q_learning_state)
         best_move_index = argmax(map(get_q, available_moves))
         return available_moves[best_move_index]
     end
-    
+
     return get_q_choice
 end
-    
-    
+
+
 
 #discount_factor (0 < 𝛾 <= 1)
 𝛾 = 0.5
 
 # learning rate (0 < ɑ <= 1)
-ɑ = 0.5
+𝛼 = 0.1
 
 
 # The state here must be:
@@ -99,20 +127,53 @@ end
 # https://en.wikipedia.org/wiki/Q-learning
 # (seems like the maxq -Q value is missing a layer of parens.
 #  https://en.wikipedia.org/wiki/Q-learning
-# q_new(state_t, action_t) = q_old(state_t, action_t) + ɑ *  (reward + 𝛾 * (argmax_actions(a-> q_old(state_t, a)) - q_old(state_t, action_t)))
+# q_new(state_t, action_t) = q_old(state_t, action_t) + 𝛼 *  (reward + 𝛾 * (argmax_actions(a-> q_old(state_t+1, a)) - q_old(state_t, action_t)))
+
+reward(x::Draw, color::Color) = 0
+reward(x::Win, color::Color)  = (x.winner == color) ? 1 : -1
 
 
-    
-function q_learn!(q_state, episodes)
 
-    ## Cacheing can be useful here
+function q_learn!(qs, episodes)
+
+    q_old(encoded_statemove) = raw_q_lookup(qs, encoded_statemove)
+
+    # Calulate what the Q-values should be based on the old q_values
+    # that are present in qs, and present them as a
+    # set of learning tuples, that can be interpreted as a
+    # learning set for the qs.chain network
+
     function learn_from_episode(episode)
-        q_old(t) = q_state.chain(episode[1][t])
-        
+        outcome, move_history, board_history = episode
+        episode_length = length(move_history)
+
+        learning_tuples = []
+        future_value_estimate = 0
+        for t in episode_length:-1:1
+            move   = move_history[t]
+            board  = board_history[t]
+            color  = get_piece_at(board, move.start).color # color == player
+            r      = t != episode_length ? 0 : reward(outcome, color)
+            esm    = one_hot_encode_chess_state(board, move)
+            new_q = q_old(esm) + 𝛼 * (r + 𝛾 * future_value_estimate)
+            future_value_estimate = new_q
+            push!(learning_tuples, [esm, one_hot_encode_q(new_q)])
+         end
+         return learning_tuples
     end
-    
-        
-    return nothing
+
+    wipe_cache!(qs)
+
+    learning_episodes = map(learn_from_episode, episodes)
+
+    # https://fluxml.ai/Flux.jl/stable/training/training/#Training-1
+    loss(x, y) = Flux.mse(qs.chain, y)
+    ps = Flux.params(qs.chain)
+    opt =  ADAM() # uses the default η = 0.001 and β = (0.9, 0.999)
+
+    for data in learning_episodes
+       Flux.train!(loss, ps, data, opt, cb = () -> println("training"))
+    end
 end
 
 
@@ -128,18 +189,18 @@ function q_learn_round(no_of_rounds = 2, no_of_episodes = 3, max_rounds_cutoff =
         # to be evolved in the future.
         Chain(
          Dense(960, 5, σ),
-         Dense(5, 100),
+         Dense(5, no_of_output_nodes_to_encode_q),
             softmax),
         Dict{Array{Bool,1},Float32}()
     )
 
     for round in 1:no_of_rounds
-        
+
         q_choice = new_q_choice(q_state)
-        
+
         # Play some games so we get something to learn from
         episodes = [play_game(q_choice, max_rounds_cutoff, devnull) for x in 1:no_of_episodes]
-        
+
         q_learn!(q_state, episodes)
     end
     return q_state
